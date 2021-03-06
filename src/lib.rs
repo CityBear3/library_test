24dabb978c51ee158672113feec27697845deb60
CityBear3 <rp0101vv@ed.ritsumei.ac.jp>
2021-03-06 11:34:25 +0900

    [update]テストコードを削除

--- src/lib.rs
+++ src/lib.rs
@@ -31,23 +31,3 @@ pub fn collatz_test(arg: u128) -> u32 {
     let n = rs_collatz(arg).unwrap() as u32;
     n
 }
-
-
-/*
-    GitHub Actions用テストコード
-    macOSでテストする場合はコメントアウトしてください
-*/
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_work() {
-        assert_eq!(1, collatz_test(999));
-    }
-
-    #[test]
-    fn it_work_2() {
-        assert_ne!(2, collatz_test(427970));
-    }
-}
