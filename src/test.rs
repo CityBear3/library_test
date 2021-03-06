4395d10e734458585dc7727b3be4b9bd3f49b8ca
CityBear3 <rp0101vv@ed.ritsumei.ac.jp>
2021-03-06 11:15:30 +0900

    [update]テストコードを修正

--- src/test.rs
+++ src/test.rs
@@ -8,6 +8,11 @@ mod tests {
     fn it_work() {
         assert_eq!(1, test_lib::collatz_test(999));
     }
+
+    #[test]
+    fn it_work_2() {
+        assert_ne!(2, test_lib::collatz_test(427970));
+    }
 }
 
 
