b4a32c4bcd56c154e9c941a7b03ab7ce64331e73
CityBear3 <rp0101vv@ed.ritsumei.ac.jp>
2021-03-06 11:28:36 +0900

    [update]GitHub Actions用のテストコードを追記

--- src/lib.rs
+++ src/lib.rs
@@ -32,3 +32,22 @@ pub fn collatz_test(arg: u128) -> u32 {
     n
 }
 
+
+/*
+    GitHub Actions用テストコード
+    macOSでテストする場合はコメントアウトしてください
+*/
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_work() {
+        assert_eq!(1, collatz_test(999));
+    }
+
+    #[test]
+    fn it_work_2() {
+        assert_ne!(2, collatz_test(427970));
+    }
+}
